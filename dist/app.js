337b70e947294a222ee4bce11d98a23b0722149c
Rohit Yadav <rohit.yadav9246@gmail.com>
2023-09-26 03:10:06 +0530

    chore: add swagger documentation for apis

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,171 @@
 "use strict";
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *   schemas:
+ *     GetMessageResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *
+ *     CreateUserRequest:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *     CreateUserResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: confirms creation of new user
+ *     LoginRequest:
+ *       type: object
+ *       properties:
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ *       required:
+ *         - email
+ *         - password
+ *     LoginResponse:
+ *       type: object
+ *       properties:
+ *         token:
+ *           type: string
+ *           description: access token user can use to access authorised apis
+ *     ErrorResponse:
+ *       type: object
+ *       properties:
+ *         error:
+ *           type: string
+ *           description: provides more detail about error
+ * /:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Message from server
+ *     description: Get a message from server.
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/GetMessageResponse'
+ *             example: { message: "Hello, Raft Labs here!" }
+ *       '401':
+ *         description: Unauthorised - due to invalid token provided or due to no token provided
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *             example: { error: "Unauthorized: Invalid token" }
+ * /create-user:
+ *   post:
+ *     summary: Create a new user
+ *     description: Create a new user with the provided information.
+ *     requestBody:
+ *       description: User data to create a new user.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example: { "name": "Jon Doe", "email": "jondoe@abcmail.com", "password": "JonDoe"}
+ *           schema:
+ *             $ref: '#/components/schemas/CreateUserRequest'
+ *     responses:
+ *       '201':
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             example: { "message": "User created successfully" }
+ *             schema:
+ *               $ref: '#/components/schemas/CreateUserResponse'
+ *       '400':
+ *         description: Bad request, invalid data provided
+ *         content:
+ *           application/json:
+ *             example: { "error": "\"email\" must be a valid email" }
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example: { "error": "An error occurred while creating the user" }
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ * /login:
+ *   post:
+ *     summary: User login
+ *     description: Authenticate a user and generate an access token.
+ *     requestBody:
+ *       description: User login data.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example: {"email": "jondoe@abcmail.com", "password": "JonDoe"}
+ *           schema:
+ *             $ref: '#/components/schemas/LoginRequest'
+ *     responses:
+ *       '200':
+ *         description: Authentication successful
+ *         content:
+ *           application/json:
+ *             example: {"token": "some JWT token"}
+ *             schema:
+ *               $ref: '#/components/schemas/LoginResponse'
+ *       '401':
+ *         description: Unauthorized, invalid email or password
+ *         content:
+ *           application/json:
+ *             example: { "error": "Invalid email or password" }
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *       '400':
+ *         description: Bad request, invalid data provided
+ *         content:
+ *           application/json:
+ *             example: {"error": "\"password\" length must be at least 6 characters long"}
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example: { "error": "An error occurred during authentication"}
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *
+ * /chatting-app:
+ *   get:
+ *     summary: get real time chat page
+ *     description: Returns a HTML file which enables user to chat suing socket.io.
+ *     responses:
+ *       '200':
+ *         description: HTML content
+ *         content:
+ *           text/html:
+ *             example: <HTML> Some Code Inside </HTML>
+ *             schema:
+ *               type: string
+ *
+ */
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
@@ -39,12 +206,35 @@ const express4_1 = require("@apollo/server/express4");
 const http_1 = __importDefault(require("http"));
 const path_1 = __importDefault(require("path"));
 const socket_io_1 = require("socket.io");
+const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 dotenv_1.default.config();
 async function startServer() {
     const app = (0, express_1.default)();
     const httpServer = http_1.default.createServer(app);
     const cache = new node_cache_1.default();
     const io = new socket_io_1.Server(httpServer);
+    const spacs = (0, swagger_jsdoc_1.default)({
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "RaftLabs Assignment Docs",
+                version: "1.0",
+                description: "This is a simple server made as an assignment for RaftLabs with Express and MongoDB.",
+                contact: {
+                    name: "Rohit Yadav",
+                    email: "rohit.yadav9246@gmail.com",
+                },
+            },
+            servers: [
+                {
+                    url: "http://localhost:3000/",
+                },
+            ],
+        },
+        apis: ["./dist/*.js"],
+    });
+    app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(spacs));
     io.use(async (socket, next) => {
         try {
             const token = socket.handshake.auth.token;
@@ -247,10 +437,11 @@ async function startServer() {
         }
     });
     function verifyToken(req, res, next) {
-        const token = req.headers.authorization.substring(7);
+        let token = req.headers.authorization;
         if (!token) {
             return res.status(401).json({ error: 'Unauthorized: No token provided' });
         }
+        token = token.substring(7);
         jsonwebtoken_1.default.verify(token, `${process.env.SECRET_KEY}`, (err, decoded) => {
             if (err) {
                 return res.status(401).json({ error: 'Unauthorized: Invalid token' });
@@ -261,7 +452,7 @@ async function startServer() {
     }
     ;
     function messageForHome() {
-        return 'Hello, Raft Labs!';
+        return 'Hello, Raft Labs here!';
     }
     app.get('/', verifyToken, (req, res) => {
         return res.json({ message: messageForHome() });
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,172 @@
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT 
+ *   schemas:
+ *     GetMessageResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ * 
+ *     CreateUserRequest:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *     CreateUserResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: confirms creation of new user 
+ *     LoginRequest:
+ *       type: object
+ *       properties:
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ *       required:
+ *         - email
+ *         - password
+ *     LoginResponse:
+ *       type: object
+ *       properties:
+ *         token:
+ *           type: string
+ *           description: access token user can use to access authorised apis
+ *     ErrorResponse:
+ *       type: object
+ *       properties:
+ *         error:
+ *           type: string
+ *           description: provides more detail about error 
+ * /:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Message from server
+ *     description: Get a message from server.
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/GetMessageResponse'
+ *             example: { message: "Hello, Raft Labs here!" }
+ *       '401':
+ *         description: Unauthorised - due to invalid token provided or due to no token provided
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *             example: { error: "Unauthorized: Invalid token" }  
+ * /create-user:
+ *   post:
+ *     summary: Create a new user
+ *     description: Create a new user with the provided information.
+ *     requestBody:
+ *       description: User data to create a new user.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example: { "name": "Jon Doe", "email": "jondoe@abcmail.com", "password": "JonDoe"}
+ *           schema:
+ *             $ref: '#/components/schemas/CreateUserRequest'
+ *     responses:
+ *       '201':
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             example: { "message": "User created successfully" }
+ *             schema:
+ *               $ref: '#/components/schemas/CreateUserResponse'
+ *       '400':
+ *         description: Bad request, invalid data provided
+ *         content:
+ *           application/json:
+ *             example: { "error": "\"email\" must be a valid email" }
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example: { "error": "An error occurred while creating the user" }
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ * /login:
+ *   post:
+ *     summary: User login
+ *     description: Authenticate a user and generate an access token.
+ *     requestBody:
+ *       description: User login data.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example: {"email": "jondoe@abcmail.com", "password": "JonDoe"}
+ *           schema:
+ *             $ref: '#/components/schemas/LoginRequest'
+ *     responses:
+ *       '200':
+ *         description: Authentication successful
+ *         content:
+ *           application/json:
+ *             example: {"token": "some JWT token"}
+ *             schema:
+ *               $ref: '#/components/schemas/LoginResponse'
+ *       '401':
+ *         description: Unauthorized, invalid email or password
+ *         content:
+ *           application/json:
+ *             example: { "error": "Invalid email or password" }
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *       '400':
+ *         description: Bad request, invalid data provided
+ *         content:
+ *           application/json:
+ *             example: {"error": "\"password\" length must be at least 6 characters long"}
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example: { "error": "An error occurred during authentication"}
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ * 
+ * /chatting-app:
+ *   get:
+ *     summary: get real time chat page
+ *     description: Returns a HTML file which enables user to chat suing socket.io.
+ *     responses:
+ *       '200':
+ *         description: HTML content
+ *         content:
+ *           text/html:
+ *             example: <HTML> Some Code Inside </HTML>
+ *             schema:
+ *               type: string
+ * 
+ */
+
+
 import express, { Express, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import * as bcrypt from 'bcryptjs';
@@ -11,6 +180,9 @@ import { expressMiddleware } from '@apollo/server/express4';
 import http from 'http';
 import path from 'path';
 import {Server, Socket} from 'socket.io';
+import swaggerjsdoc from 'swagger-jsdoc';
+import swaggerui from 'swagger-ui-express';
+
 dotenv.config();
 
 async function startServer() {
@@ -19,6 +191,29 @@ async function startServer() {
   const httpServer = http.createServer(app);
   const cache = new NodeCache();
   const io = new Server(httpServer);
+  
+  const spacs = swaggerjsdoc({
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "RaftLabs Assignment Docs",
+        version: "1.0",
+        description: "This is a simple server made as an assignment for RaftLabs with Express and MongoDB.",
+        contact: {
+          name: "Rohit Yadav",
+          email: "rohit.yadav9246@gmail.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000/",
+        },
+      ],
+    },
+    apis: ["./dist/*.js"],
+  });
+
+  app.use("/api-docs", swaggerui.serve, swaggerui.setup(spacs) );
 
   io.use(async (socket, next)=>{
     try{
@@ -300,11 +495,12 @@ async function startServer() {
 
 
   function verifyToken(req: any, res: any, next: any) {
-    const token = req.headers.authorization.substring(7);
-
+    let token = req.headers.authorization
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized: No token provided' });
     }
+
+    token = token.substring(7);
 
     jwt.verify(token, `${process.env.SECRET_KEY}`, (err: any, decoded: any) => {
       if (err) {
@@ -317,7 +513,7 @@ async function startServer() {
 
 
   function messageForHome(){
-    return 'Hello, Raft Labs!';
+    return 'Hello, Raft Labs here!';
   }
 
   app.get('/', verifyToken, (req: Request, res: Response) => {
